b29428c7df156d5691fa389e0bdafdf9f6fafa37
Aleksandr Tukallo <alex.tukallo@gmail.com>
2016-11-30 11:22:25 +0300

    Emty project created

---
+++ app/src/androidTest/java/com/project/itmo2016/edutrackerapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.project.itmo2016.edutrackerapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/project/itmo2016/edutrackerapplication/ChooseGroupActivity.java
@@ -0,0 +1,13 @@
+package com.project.itmo2016.edutrackerapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_group);
+    }
+}
